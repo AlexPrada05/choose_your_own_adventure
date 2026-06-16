095fa9c89229ec707bbe1c182f1392aa970501db
AlexPrada05 <babauprada11@gmail.com>
2026-06-16 11:51:43 +0200

    Update main.py
    Prepared with FastAPI and her security with middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+
+app = FastAPI(
+    title="Choose Your Own Adventrue Game API",
+    description="api to generate cool stories",
+    version="1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+# Adding this middleware here, called CORS (Cross-Origin Resource Sharing)
+# So we´re allowed to enable certain origin. So certain URLs to interact with our backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], # Like GET(retrieving data), POST(making date), PUT(updating data)
+    allow_headers=["*"], # additional information we can send with the request
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
