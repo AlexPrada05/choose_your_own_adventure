a5da1c98aa85b175302c1e2e0c1c2989e1f8bb93
AlexPrada05 <babauprada11@gmail.com>
2026-06-16 12:26:48 +0200

    Database setup
    Making the connection and the creation of tables

---
+++ backend/models/story.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+from backend.db.database import Base
+
+class Story():
+    pass
