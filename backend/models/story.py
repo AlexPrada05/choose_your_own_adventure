2c02319bdea5110326a8e8c3943ad7e316cecca3
AlexPrada05 <babauprada11@gmail.com>
2026-06-16 12:32:14 +0200

    Updated story.py
    - I´ve made the fist node that is Story within his fileds and his primary_key

--- backend/models/story.py
+++ backend/models/story.py
@@ -4,5 +4,13 @@ from sqlalchemy.orm import relationship
 
 from backend.db.database import Base
 
-class Story():
-    pass
+class Story(Base):
+    __table__ = "stories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    session_id = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    nodes = relationship("StoryNode", back_populates="story")
+
